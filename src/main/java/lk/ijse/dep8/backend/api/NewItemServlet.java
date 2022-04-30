a1c63aba0e0bcc5bace465cee3840882d13f97f6
LahiruRJ <sanlahiru720@gmail.com>
2022-04-30 22:09:54 +0530

    Add newSevlet

---
+++ src/main/java/lk/ijse/dep8/backend/api/NewItemServlet.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep8.backend.api;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "NewItemServlet", value = "/NewItemServlet")
+public class NewItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
