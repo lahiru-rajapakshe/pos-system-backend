1438a1069e3623a68b98dc8509649ad04a7b9ee1
LahiruRJ <sanlahiru720@gmail.com>
2022-04-30 19:37:31 +0530

    Finalize and test the connection pool configurations

---
+++ src/main/java/lk/ijse/dep8/backend/TestServlet.java
@@ -0,0 +1,24 @@
+package lk.ijse.dep8.backend;
+
+import javax.annotation.Resource;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@WebServlet(name = "TestServlet", value = "/TestServlet")
+public class TestServlet extends HttpServlet {
+    @Resource(name = "java:comp/env/jdbc/pool4pos")
+    private volatile DataSource pool;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
